e81d147ee542dc1a1ec8ecebb5b967ab128fdf4a
abdullah <abody.hafiz2@gmail.com>
2026-05-12 21:34:28 +0200

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -3,7 +3,7 @@
  using namespace std;
 
 int main(){
-   int array [] = (65,4,98,18,23,56,78,90,12);
+   int array [] = (62,8,95,11,30,45,80,89,22);
    int n = 9;
     for (int i = 0; i < n-1; i++)
     {
